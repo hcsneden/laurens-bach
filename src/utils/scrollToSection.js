a533d76ea31253b86652115abec357b895e45423
hsneden <48335502+hcsneden@users.noreply.github.com>
2026-06-16 09:05:06 -0600

    fixing routing

--- src/utils/scrollToSection.js
+++ src/utils/scrollToSection.js
@@ -6,6 +6,5 @@ export function scrollToSection(e, href) {
   const element = document.querySelector(href)
   if (element) {
     element.scrollIntoView({ behavior: 'smooth' })
-    history.pushState(null, '', href)
   }
 }
